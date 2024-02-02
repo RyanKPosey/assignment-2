dce13776f417a781c34a072a3875520b5cff92b4
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2024-02-02 03:17:48 +0000

    Complete problem 9

---
+++ p9.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char asciiValue;
+    cout << "Enter a character: ";
+    cin >> asciiValue;
+
+    cout << endl << (int)asciiValue << endl;
+}
