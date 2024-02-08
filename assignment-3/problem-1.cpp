45e3557efdbc9e199838b58f34502563da62f747
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2024-02-08 02:56:46 +0000

    Complete problem-1

---
+++ assignment-3/problem-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main () {
+    double x, y, area;
+
+    cout << "Enter a number for the base of the triangle: ";
+    cin >> x;
+
+    cout << "\nEnter a number for the height of the triangle: ";
+    cin >> y;
+
+    area = (x * y) / 2;
+    cout << endl << area << endl;
+}
