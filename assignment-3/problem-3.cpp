fc4ad8bd20029b1bdd3a4f2d2b6a2a27be5c887c
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2024-02-08 03:08:39 +0000

    Complete problem-3.cpp

--- assignment-3/problem-3.cpp
+++ assignment-3/problem-3.cpp
@@ -1,1 +1,18 @@
-c
+#include <iostream>
+using namespace std;
+int main () {
+    float x, y, z, avg;
+
+    cout << "Enter a number for X: ";
+    cin >> x;
+
+    cout << "\nEnter a number for Y: ";
+    cin >> y;
+
+    cout << "\nEnter a number for Z: ";
+    cin >> z;
+
+    avg = (x + y + z) / 3;
+    
+    cout << "\nThe average of your 3 numbers is: " << avg << endl;
+}
