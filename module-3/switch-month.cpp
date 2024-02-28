4a74d8506a5dc9d940c2d6a6f5879e0a13d7b45b
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2024-02-28 03:25:49 +0000

    Print days in month based on month number

--- module-3/switch-month.cpp
+++ module-3/switch-month.cpp
@@ -10,55 +10,19 @@ int main() {
     cout << endl;
 
     switch (monthNum) {
-        case 1:
-            month = "January";
-            daysInMonth = 31;
-            break;
         case 2:
-            month = "February";
             daysInMonth = 28;
             break;
-        case 3:
-            month = "March";
-            daysInMonth = 31;
-            break;
         case 4:
-            month = "April";
-            daysInMonth = 30;
-            break;
-        case 5:
-            month = "May";
-            daysInMonth = 31;
-            break;
         case 6:
-            month = "June";
-            daysInMonth = 30;
-            break;
-        case 7:
-            month = "July";
-            daysInMonth = 31;
-            break;
-        case 8:
-            month = "August";
-            daysInMonth = 31;
-            break;
         case 9:
-            month = "September";
-            daysInMonth = 30;
-            break;
-        case 10:
-            month = "October";
-            daysInMonth = 31;
-            break;
         case 11:
-            month = "November";
             daysInMonth = 30;
             break;
-        case 12:
-            month = "December";
+        default:
             daysInMonth = 31;
             break;
     }
 
-    cout << "It's " << month << endl;
+    cout << "This month has " << daysInMonth << " days" << endl;
 }
