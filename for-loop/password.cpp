37fda8cd9179201f8e3132f9ef6cac0a6e6440da
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2024-03-08 03:14:43 +0000

    Add password.cpp

---
+++ for-loop/password.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string password = "password";\
+    string usrPassword;
+
+    cout << "Enter your password: ";
+    cin >> usrPassword;
+    
+    cout << endl;
+
+    for (; usrPassword != password; NULL) {
+        cout << "Incorrect password, try again\n";
+        cout << "Password: ";
+        cin >> usrPassword;
+        cout << endl;
+    }
+
+    cout << "Correct, you're in. \n";
+}
