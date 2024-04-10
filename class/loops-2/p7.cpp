6120e6e44ea90e4f1cffd57037b7bb293edeede7
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2024-04-10 03:43:31 +0000

    Fix p7.cpp

--- class/loops-2/p7.cpp
+++ class/loops-2/p7.cpp
@@ -6,7 +6,7 @@ int main() {
     cout << "Enter n: ";
     cin >> n;
 
-    for (int i = 0; i <= n; i++) {
+    for (int i = 1; i <= n; i++) {
         for (int i2 = 1; i2 <= i; i2++) {
             cout << i2;
         }
