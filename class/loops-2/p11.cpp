aabecf91edbe2b8bc7999cfacabd30609ff488dd
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2024-04-12 03:27:17 +0000

    Add p11.cpp

---
+++ class/loops-2/p11.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nums = "";
+    cout << "Enter a string of numbers: ";
+    getline(cin, nums);
+
+    cout << endl;
+
+    string result = "";
+
+    for (int i = 0; i < nums.length(); i++) {
+        bool recordNum = true;
+        for (int i2 = i+1; i2 < nums.length(); i2++) {
+            if (nums[i2] < nums[i]) {
+                recordNum = false;
+            }
+        }
+        if (recordNum) {
+            result += nums[i];
+        }
+    }
+
+    cout << result << endl;
+}
