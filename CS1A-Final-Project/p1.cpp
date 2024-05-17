dfcf6c7f6bdd4f110a25284bce3144ac216a5b56
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2024-05-17 07:56:52 +0000

    Add displayTasks()

--- CS1A-Final-Project/p1.cpp
+++ CS1A-Final-Project/p1.cpp
@@ -10,6 +10,13 @@ void markAsCompleted(string& task) {
     task += " [completed]";
 }
 
+void displayTasks(string tasks[], int tasksLength) {
+    cout << "Todo List: \n";
+    for (int i = 0; i < tasksLength; i++) {
+        cout << i+1 << ": " << tasks[i] << endl;
+    }
+}
+
 int main() {
     string tasks[] = {};
     int tasksLength = 0;
